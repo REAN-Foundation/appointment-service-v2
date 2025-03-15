47939add4719f45baf967fc328c807723069bbaf
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2025-03-15 14:03:27 +0530

    modified entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Add config/creds copying here..
 aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/env.config /app/.env
 aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/internal.clients.seed.json /app/seed.data/internal.clients.seed.json
-aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/internal.test.users.seed.json /app/seed.data/internal.test.users.seed.json
+aws s3 cp s3://$S3_CONFIG_BUCKET/$S3_CONFIG_PATH/seed_data/default.users.seed.json /app/seed.data/default.users.seed.json
 
 cd /app
 # Add any other scripts here...
